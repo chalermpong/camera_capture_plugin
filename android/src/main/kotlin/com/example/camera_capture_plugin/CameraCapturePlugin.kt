295115ca06f8fa90bf5384ac1f79ad7231e41a97
chalermpong <chalermpong@ltman.com>
2025-06-09 21:43:51 +0700

    Implement android code

--- android/src/main/kotlin/com/example/camera_capture_plugin/CameraCapturePlugin.kt
+++ android/src/main/kotlin/com/example/camera_capture_plugin/CameraCapturePlugin.kt
@@ -20,11 +20,34 @@ class CameraCapturePlugin: FlutterPlugin, MethodCallHandler {
   }
 
   override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    switch (call.method) {
+      "getPlatformVersion" -> result.success("Android ${android.os.Build.VERSION.RELEASE}")
+      "handleCameraImage" -> handleCameraImage(call, result)
+      else -> result.notImplemented()
     }
+  }
+
+  private fun handleCameraImage(call: MethodCall, result: MethodChannel.Result) {
+    val arg = (call.arguments as? Map<String, *>)
+    val bytes: ByteArray? = arg?.get("planes") as? ByteArray
+    val width: Int? = arg?.get("width") as? Int
+    val height: Int? = arg?.get("height") as? Int
+    val quality: Int = 85
+
+    if(bytes == null || width == null || height == null) {
+      result.error("Null argument", "bytes, width, height must not be null", null)
+      return
+    }
+
+    Thread {
+      val out = ByteArrayOutputStream()
+      val yuv = YuvImage(bytes, ImageFormat.NV21, width, height, null)
+      yuv.compressToJpeg(Rect(0, 0, width, height), quality, out)
+      val converted = out.toByteArray()
+      Handler(Looper.getMainLooper()).post {
+        result.success(converted)
+      }
+    }.start()
   }
 
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
